ee538b69e29b453d9b390b16088dc2d7e3feb3e9
BBFerdl <muehlbachler.dominik@gmail.com>
2022-09-18 16:19:01 +0200

    fixed x/y confusion

--- Lshape.py
+++ Lshape.py
@@ -26,23 +26,24 @@ shapes = [[[1,1],
        
        [[1,1,1],
         [0,0,1]]]
-shapesList = [0,1,2,3,4,5,6,7]
 #Grundfeld, entweder 4,6 oder 8,8
 #Leider fürt die Multiplikation der Felder zu Fehlern
-rows = 4
-cols = 6
+rows = 16
+cols = 8
 #Startposition List for Randomization
 posX = [0]
 posY = [0]
 x = 1
 y = 1
-while y <= (rows-len(shapes[0])+1):
+while y <= (rows-len(shapes[7][0])+1):
     posY.append(y)
     y = y + 1
     
-while x <= (cols-len(shapes[7][0])+1):
+while x <= (cols-len(shapes[0])+1):
     posX.append(x)
     x = x + 1
+print(posX)
+print(posY)
 #Zähler um zu messen wann das Feld voll ist.
 counter = 0
 upperLimit = (rows*cols)/4
@@ -61,16 +62,18 @@ def createField(row,col):
     return outer
 
 def fit(checkShape, checkField, checkPosX, checkPosY):
-    if (checkPosX + len(checkShape) >= len(checkField)):
+    print(str(checkPosX)+str(checkPosY))
+    print(checkField)
+    if (checkPosY + len(checkShape[0]) >= len(checkField)):
         return 0
-    if (checkPosY + len(checkShape[0]) >= len(checkField[0])):
+    if (checkPosX + len(checkShape) >= len(checkField[0])):
         return 0
     i = 0
     while i < len(checkShape):
         j = 0
         while j < len(checkShape[i]):
             if checkShape[i][j] != 0:
-                if checkField[i + checkPosX][j + checkPosY] != 0:
+                if checkField[i + checkPosY][j + checkPosX] != 0:
                     return 0
             j += 1
         i += 1
@@ -98,7 +101,7 @@ def fill(field, objectsPlaced):
                         j = 0
                         while j < len(shape[i]):
                             if shape[i][j] != 0:
-                                copyField[i + positionCol][j + positionRow] = objectsPlaced
+                                copyField[i + positionRow][j + positionCol] = objectsPlaced
                             j += 1
                         i += 1
                     if objectsPlaced >= upperLimit:
