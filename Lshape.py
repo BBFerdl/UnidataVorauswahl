04be4f44ff1607719b0db82092717fb4365ea538
BBFerdl <muehlbachler.dominik@gmail.com>
2022-09-18 14:52:33 +0200

    changed declaration of array
    now the l shapes can be printed as instructed
    there is an issue with boundaries and with the set up of the recursion

--- Lshape.py
+++ Lshape.py
@@ -28,9 +28,9 @@ shapes = [[[1,1],
         [0,0,1]]]
 shapesList = [0,1,2,3,4,5,6,7]
 #Grundfeld, entweder 4,6 oder 8,8
+#Leider fürt die Multiplikation der Felder zu Fehlern
 rows = 4
 cols = 6
-globalField = [[0]*cols]*rows
 #Startposition List for Randomization
 posX = [0]
 posY = [0]
@@ -47,69 +47,72 @@ while x <= (cols-len(shapes[7][0])+1):
 counter = 0
 upperLimit = (rows*cols)/4
 
+def createField(row,col):
+    outer = []
+    i = 0
+    while i < row:
+        inner = [0]
+        j = 0
+        while j < col-1:
+            inner.append(0)
+            j += 1
+        outer.append(inner)
+        i += 1
+    return outer
+
 def fit(checkShape, checkField, checkPosX, checkPosY):
-    
-    if (checkPosY + len(checkShape) >= len(checkField[0])):
+    if (checkPosX + len(checkShape) >= len(checkField)):
         return 0
-    if (checkPosX + len(checkShape[0]) >= len(checkField)):
+    if (checkPosY + len(checkShape[0]) >= len(checkField[0])):
         return 0
     i = 0
-    j = 0
     while i < len(checkShape):
+        j = 0
         while j < len(checkShape[i]):
             if checkShape[i][j] != 0:
                 if checkField[i + checkPosX][j + checkPosY] != 0:
                     return 0
-            j = j + 1
-        i = i + 1
+            j += 1
+        i += 1
     return 1        
 
 def fill(field, objectsPlaced):
-    print (posX)
-    copyField = [[0]*cols]*rows
+    copyField = createField(rows,cols)
     i = 0
-    j = 0
     while i < len(field):
+        j = 0
         while j < len(field[i]):
             copyField[i][j] = field[i][j]
-            j = j + 1
-        i = i + 1
+            j += 1
+        i += 1
     random.shuffle(shapes)
     random.shuffle(posX)
     random.shuffle(posY)
     for shape in shapes:
-        for positionRow in posY:
-            for positionCol in posX:
-                if fit(shape, copyField, positionRow, positionCol):
+        for positionCol in posX:
+            for positionRow in posY:
+                if fit(shape, copyField, positionCol, positionRow):
                     objectsPlaced = objectsPlaced + 1
                     i = 0
-                    j = 0
-                    c = 0
-                    print (shape)
                     while i < len(shape):
+                        j = 0
                         while j < len(shape[i]):
-                            print(i + positionCol)
                             if shape[i][j] != 0:
-                                c = c + 1
-                                copyField[i + positionRow][j + positionCol] = objectsPlaced
-                            j = j + 1
-                        i = i + 1
-                    print(c)
+                                copyField[i + positionCol][j + positionRow] = objectsPlaced
+                            j += 1
+                        i += 1
                     if objectsPlaced >= upperLimit:
                         return copyField
                     else: 
-                        return fill(copyField, objectsPlaced)
-                        
-                    
-                        
+                        return fill(copyField, objectsPlaced)                              
     return field
 
-
 objectsPlaced = 0
-initialField = [[0]*cols]*rows
+
+initialField = createField(rows,cols)
 result = fill(initialField, objectsPlaced)
-for column in result:
-    print (column)
+for row in result:
+    print (row)
 print("finished")
                     
 
